0061a932b1a51d080591e4307bcc063669ebe8c8
Monojit Saha <monojitsaha@Monojits-MacBook-Pro.local>
2026-03-14 22:02:50 +0530

    chore: code setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# event-driven-ecommerce-api
-NestJS backend for an event-driven e-commerce POC. Handles product retrieval, Stripe checkout session creation, and webhook processing. Payment events trigger asynchronous order workflows using Redis queues with MongoDB persistence, designed for containerized deployment.
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const port = process.env.PORT || 8080
+  const app = await NestFactory.create(AppModule);
+
+  Logger.log('Environment: Dev', 'Bootstarp');
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('The Blog Post Project')
+    .setDescription('Open api for the blog post project')
+    .setVersion('v1')
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port);
+  Logger.log(`Server running on port ${port}`, 'Bootstrap');
+}
+
+bootstrap();
